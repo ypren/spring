5b0944b173ebc6d49346496216bbbc0482d0d0d9
Yongping Ren <yongping.ren@ygomi.com>
2019-05-30 15:25:14 +0800

    add global controller advice

--- spring-boot/src/main/java/com/ypren/springboot/controller/UserController.java
+++ spring-boot/src/main/java/com/ypren/springboot/controller/UserController.java
@@ -1,8 +1,11 @@
-package com.ypren.springboot.Controller;
+package com.ypren.springboot.controller;
 
+import com.ypren.springboot.model.User;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 /**
  * Simple rest controller.
@@ -17,5 +20,11 @@ public class UserController {
     public String version() {
         log.info("request version -> {}", VERSION);
         return VERSION;
+    }
+
+    @PostMapping(path = "/api/user")
+    public Long addUser(@RequestParam User user) {
+        // todo post an user into database;
+        return null;
     }
 }
