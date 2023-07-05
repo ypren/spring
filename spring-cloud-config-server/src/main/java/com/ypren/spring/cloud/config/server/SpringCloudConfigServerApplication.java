8acd0ca15efc41b3f070fd0432302b58d4eb3dd8
Yongping Ren <yongping@qti.qualcomm.com>
2023-07-05 11:18:53 +0800

    add spring-cloud-config-server

---
+++ spring-cloud-config-server/src/main/java/com/ypren/spring/cloud/config/server/SpringCloudConfigServerApplication.java
@@ -0,0 +1,22 @@
+package com.ypren.spring.cloud.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+@EnableConfigServer
+public class SpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+	}
+
+	@RequestMapping("/spring-cloud-config-server")
+	public String hello() {
+		return "Hello, I'm Config Server!";
+	}
+}
